1bad9bf6cdf4807479d0233ee0c90246d1b75b49
Tahmid Hamim <tahmidhamim@gmail.com>
2024-03-26 16:32:09 +0600

    Init commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+const address = process.argv[2];
+
+if (!address) {
+    console.log('Please provide an address');
+} else {
+    geocode(address, (error, { location, latitude, longitude } = {}) => {
+        if (error) {
+            return console.log(error);
+        }
+
+        forecast(latitude, longitude, (error, data) => {
+            if (error) {
+                return console.log(error);
+            }
+    
+            console.log(location);
+            console.log(data);
+        });
+    });
+}
